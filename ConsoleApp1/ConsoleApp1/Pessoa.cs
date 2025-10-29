1c12b002832c5115e0189b3782d1d7f8667cb784
unknown <lab1069@etecjk.local>
2025-10-29 19:18:00 -0300

    banco de dados

---
+++ ConsoleApp1/ConsoleApp1/Pessoa.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+     class Pessoa
+    {
+        public Pessoa() {}
+        public int id { get; set; }
+        public string Nome { get; set; }
+        public string Senha { get; set; }
+        public Pessoa(string n)
+        {
+            this.Nome = n;
+        }
+
+        public Pessoa(string n,string s)
+        {
+            this.Nome = n;
+            this.Senha = s;
+        }
+        public void CadastrarUsuario()
+        {
+            Console.WriteLine("qual seu numero de identificacao?");
+            this.id = int.Parse(Console.ReadLine());
+            Console.WriteLine("qual seu nome");
+            this.Nome = Console.ReadLine();
+            Console.WriteLine("qual sua senha");
+            this.Senha = Console.ReadLine();
+
+        }
+   
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MySqlCommand cmd;
+            MySqlConnection conexao;
+            string sql;
+            conexao = new MySqlConnection("server=127.0.0.1;uid=root;pwd=root;database=teste01");
+            try
+            {
+                conexao.Open();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("erro ao conectar no banco de dados" + ex.Message);
+            }
+
+            Pessoa p = new Pessoa();
+            p.CadastrarUsuario();
+
+            sql = "insert into Login1 values(" + p.id + ",'" + p.Nome + "','" + p.Senha + "');";
+            cmd = new MySqlCommand(sql, conexao);
+            int linhasafetadas;
+            linhasafetadas = cmd.ExecuteNonQuery();
+            Console.WriteLine("Linhas afetadas:{0}",linhasafetadas);
+
+            sql = "Select * from Login1";
+            cmd = new MySqlCommand(sql, conexao);
+            MySqlDataReader rdr = cmd.ExecuteReader();
+           
+            while (rdr.Read())
+            {
+                Console.WriteLine(rdr["id"] + " -- " + rdr["usuario"] + " -- " + rdr[2]);
+            }
+            rdr.Close();
+            conexao.Close();
+
+            
+        }
+    }
+}
